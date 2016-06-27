53ce26e74eecb41e8167aa900bb92eb05e63a26a
Administrator <Administrator@MS-20150820FMEW>
2016-06-27 21:44:17 +0800

    commit before synchronization

---
+++ src/CompCtrl/StepOutTarget.java
@@ -0,0 +1,105 @@
+package CompCtrl;
+
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.lin;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.linRel;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.kuka.common.ThreadUtil;
+
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.CartDOF;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.math.Transformation;
+import com.kuka.roboticsAPI.motionModel.HandGuidingMotion;
+import com.kuka.roboticsAPI.motionModel.PositionHold;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.JointImpedanceControlMode;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.PositionControlMode;
+import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
+
+/**
+ * Creates a FRI Session.
+ */
+public class StepOutTarget extends RoboticsAPIApplication
+{
+    private Controller _lbrController;
+    private LBR _lbr;
+    private String _clientName;
+    private Tool _Gripper;
+    
+    //Impedance para.
+    /*private static final int stiffnessZ = 0;  	//unit: N/m. Default: 2000
+	private static final int stiffnessY = 0;
+	private static final int stiffnessX = 0;
+	
+	private static final int StiffnessRot = 0;  	//unit: Nm/rad  Default: 300*/
+    private static final double StiffnessTran = 3000;  	//unit: N/m. Default: 2000
+	private static final double StiffnessRot = 300;  	//unit: Nm/rad  Default: 200
+    
+	private static final double MaxForceTCP = 0.1;		//unit: N
+	private static final double MaxTorqueTCP = 0.1;  	//unit: Nm
+	
+	private final static String informationText="More Z-dir move?";
+
+    @Override
+    public void initialize()
+    {
+        _lbrController = (Controller) getContext().getControllers().toArray()[0];
+        _lbr = (LBR) _lbrController.getDevices().toArray()[0];
+        
+        //copy tool information "Gripper" to controller
+        _Gripper = getApplicationData().createFromTemplate("Gripper");
+    }
+
+    @Override
+    public void run()
+    {
+    	//use tool
+    	_Gripper.attachTo(_lbr.getFlange());
+   	
+    	CartesianImpedanceControlMode impedanceControlMode = 	new CartesianImpedanceControlMode();
+		//impedanceControlMode.parametrize(CartDOF.X, CartDOF.Y, CartDOF.Z).setStiffness(StiffnessTran);
+		//impedanceControlMode.parametrize(CartDOF.ROT).setStiffness(StiffnessRot);
+    	impedanceControlMode.setMaxControlForce(MaxForceTCP, MaxForceTCP, MaxForceTCP, MaxTorqueTCP, MaxTorqueTCP, MaxTorqueTCP, true);  
+        
+    	
+    	
+    	
+    	_Gripper.getFrame("/CompCenter").move(linRel(Transformation.ofDeg(0,0,10,0,0,0),getApplicationData().getFrame("/BaseFrame")).setCartVelocity(100.0).setMode(impedanceControlMode));
+    	
+    	//Ensure gripper safety
+    	int isCancel = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, informationText, "Yes", "No");
+        while (isCancel == 0)
+        {
+        	_Gripper.getFrame("/CompCenter").move(linRel(Transformation.ofDeg(0,0,10,0,0,0),getApplicationData().getFrame("/BaseFrame")).setCartVelocity(100.0).setMode(impedanceControlMode));
+        	getLogger().info("Step Move up.");
+        	isCancel = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, informationText, "Yes", "No");
+        	if (isCancel == 1)
+        	{
+        		break;
+        	}
+        }
+    	
+        getLogger().info("start Postionhold.");       
+        PositionHold posHold = new PositionHold(impedanceControlMode, 60, TimeUnit.DAYS);
+        _Gripper.getFrame("/CompCenter").move(posHold);
+    }
+
+    /**
+     * main.
+     * 
+     * @param args
+     *            args
+     */
+    public static void main(final String[] args)
+    {
+        final StepOutTarget app = new StepOutTarget();
+        app.runApplication();
+    }
+
+}
