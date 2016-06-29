769c6388a2c3025fa190ea248758f028a08ce1bf
Administrator <Administrator@MS-20150820FMEW>
2016-06-29 09:48:57 +0800

    commit before synchronization

--- src/CompCtrl/GraspComp.java
+++ src/CompCtrl/GraspComp.java
@@ -72,7 +72,7 @@ public class GraspComp extends RoboticsAPIApplication
     	
     	//Record data
     	DataRecorder rec = new DataRecorder();
-    	rec.setFileName("Recording4.log");
+    	rec.setFileName("Recording5.log");
     	rec.setSampleInterval(10);
     	rec.setTimeout(1000, TimeUnit.HOURS);
     	
